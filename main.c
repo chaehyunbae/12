084cf9f08bce3b1e362027e1bbeef80f109ec649
chaehyunbae <gotch9359@naver.com>
2020-11-21 22:39:52 +0900

    07

--- main.c
+++ main.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
 	FILE *fp;
-	char c;
+	char str[100];
+	char path[100];
+	char lookup[100];
 	
-	fp= fopen("sample. txt", "r");
-	if( fp ==NULL)
+	printf("input file path: ");
+	scanf("%s", path);
+	printf("input search word:");
+	scanf("%s", lookup);
+	
+	fp = fopen(path, "r");
+	if (fp == NULL)
 	{
+		printf("invalid path! (%s)\n", path);
 		return -1;
-	}
-	
-	while ((c=fgetc(fp)) != EOF) {
-		printf("%c", c);
 		
 	}
+	while (fgets(str,100, fp) != NULL){
+		
+		if (strncmp(str, lookup, strlen(lookup))==0)
+		{
+			printf("Search succeed!\n");
+			fclose(fp);
+			return 0;
+		}
+	}
 	
+	printf("Search failed!\n");
 	fclose(fp);
+	
 	
 	return 0;
 }
